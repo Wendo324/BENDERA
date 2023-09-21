1315f4cd9845b18eddb4a6470562033e25dcd295
WENDO GITHAKA <githakawendo@students.uonbi.ac.ke>
2023-09-21 13:46:44 +0300

    gg

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 'use strict'
 
 //these are my questions and choices//
@@ -503,15 +512,7 @@ function theResultsAreIn(correctAnswers) {
     </section>
   `); 
 }
-/* Open when someone clicks on the span element */
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
 
 //this runs the stuff//
 function callButtons() {
