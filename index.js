21b8be876fdbb0e116454b6e7cf2a3a761302945
WENDO GITHAKA <githakawendo@students.uonbi.ac.ke>
2023-09-21 07:23:18 +0300

    video overlay

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-'use strict'
+<script>
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
+    'use strict'
 
 //these are my questions and choices//
 const myQuestions = [
