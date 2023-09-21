efdbed94e8821fe409c85ffb7d381531d49d4d43
WENDO GITHAKA <githakawendo@students.uonbi.ac.ke>
2023-09-21 07:42:59 +0300

    debug

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-<script>
-// Get the video
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
-</script>
-    'use strict'
+'use strict'
 
 //these are my questions and choices//
 const myQuestions = [
